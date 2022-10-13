de51384150672a1741e83176766cb8e01a65f19c
Yixuan Qiu <yixuanq@gmail.com>
2022-10-13 20:37:35 +0800

    update R interface

--- R/l3solver.R
+++ R/l3solver.R
@@ -1,15 +1,29 @@
-l3solver = function(Umat, Vmat, Amat = NULL, bvec = NULL, max_iter = 1000, tol = 1e-5, verbose = FALSE)
+l3solver = function(
+    Xmat, Umat, Vmat, Smat = NULL, Tmat = NULL, tau = Inf,
+    Amat = NULL, bvec = NULL,
+    max_iter = 1000, tol = 1e-5, verbose = FALSE)
 {
-    K = length(Umat)
-    n = nrow(Umat[[1]])
-    d = ncol(Umat[[1]])
+    n = nrow(Xmat)
+    d = ncol(Xmat)
+
+    if(is.null(Umat) || is.null(Vmat))
+    {
+        Umat = Vmat = matrix(0, 0, n)
+    }
+    if(is.null(Smat) || is.null(Tmat))
+    {
+        Smat = Tmat = matrix(0, 0, n)
+    }
     if(is.null(Amat) || is.null(bvec))
     {
         Amat = matrix(0, 0, d)
         bvec = numeric(0)
     }
 
-    l3solver_(Umat, Vmat, Amat, bvec, max_iter, tol, verbose)
+    l3solver_(
+        Xmat, Amat, bvec, Umat, Vmat, Smat, Tmat, tau,
+        max_iter, tol, verbose
+    )
 }
 
 l3cd = function(Umat, Vmat, Amat = NULL, bvec = NULL, max_iter = 1000, tol = 1e-5, verbose = FALSE)
