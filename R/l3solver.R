3576d5a2e2ec8cb0fb7e4d4cfd58cdff8b2aace1
Yixuan Qiu <yixuanq@gmail.com>
2022-12-22 10:34:18 +0800

    no longer needed

--- R/l3solver.R
+++ R/l3solver.R
@@ -29,17 +29,3 @@ l3solver = function(
         max_iter, tol, verbose
     )
 }
-
-l3cd = function(Umat, Vmat, Amat = NULL, bvec = NULL, max_iter = 1000, tol = 1e-5, verbose = FALSE)
-{
-    K = length(Umat)
-    n = nrow(Umat[[1]])
-    d = ncol(Umat[[1]])
-    if(is.null(Amat) || is.null(bvec))
-    {
-        Amat = matrix(0, 0, d)
-        bvec = numeric(0)
-    }
-
-    l3cd_(Umat, Vmat, Amat, bvec, max_iter, tol, verbose)
-}
