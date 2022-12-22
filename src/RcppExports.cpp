651fa8e6d15f537187a9fff1d024e2ca5b3c59bc
Yixuan Qiu <yixuanq@gmail.com>
2022-12-22 10:34:48 +0800

    auto-generated

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -12,8 +12,8 @@ Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
 #endif
 
 // l3solver
-List l3solver(NumericMatrix Xmat, NumericMatrix Amat, NumericVector bvec, NumericMatrix Umat, NumericMatrix Vmat, NumericMatrix Smat, NumericMatrix Tmat, double tau, int max_iter, double tol, bool verbose);
-RcppExport SEXP _L3solver_l3solver(SEXP XmatSEXP, SEXP AmatSEXP, SEXP bvecSEXP, SEXP UmatSEXP, SEXP VmatSEXP, SEXP SmatSEXP, SEXP TmatSEXP, SEXP tauSEXP, SEXP max_iterSEXP, SEXP tolSEXP, SEXP verboseSEXP) {
+List l3solver(NumericMatrix Xmat, NumericMatrix Amat, NumericVector bvec, NumericMatrix Umat, NumericMatrix Vmat, NumericMatrix Smat, NumericMatrix Tmat, NumericMatrix TauMat, int max_iter, double tol, bool verbose);
+RcppExport SEXP _L3solver_l3solver(SEXP XmatSEXP, SEXP AmatSEXP, SEXP bvecSEXP, SEXP UmatSEXP, SEXP VmatSEXP, SEXP SmatSEXP, SEXP TmatSEXP, SEXP TauMatSEXP, SEXP max_iterSEXP, SEXP tolSEXP, SEXP verboseSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -24,11 +24,11 @@ BEGIN_RCPP
     Rcpp::traits::input_parameter< NumericMatrix >::type Vmat(VmatSEXP);
     Rcpp::traits::input_parameter< NumericMatrix >::type Smat(SmatSEXP);
     Rcpp::traits::input_parameter< NumericMatrix >::type Tmat(TmatSEXP);
-    Rcpp::traits::input_parameter< double >::type tau(tauSEXP);
+    Rcpp::traits::input_parameter< NumericMatrix >::type TauMat(TauMatSEXP);
     Rcpp::traits::input_parameter< int >::type max_iter(max_iterSEXP);
     Rcpp::traits::input_parameter< double >::type tol(tolSEXP);
     Rcpp::traits::input_parameter< bool >::type verbose(verboseSEXP);
-    rcpp_result_gen = Rcpp::wrap(l3solver(Xmat, Amat, bvec, Umat, Vmat, Smat, Tmat, tau, max_iter, tol, verbose));
+    rcpp_result_gen = Rcpp::wrap(l3solver(Xmat, Amat, bvec, Umat, Vmat, Smat, Tmat, TauMat, max_iter, tol, verbose));
     return rcpp_result_gen;
 END_RCPP
 }
