fb68627e068a33e0b7472c373f138eb68b534b81
statmlben <bendaimath@gmail.com>
2023-04-10 20:30:34 +0800

    #QR: elastic net

--- python/benchmark/QR/test.py
+++ python/benchmark/QR/test.py
@@ -13,8 +13,11 @@ import time
 import seaborn as sns
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
+from funs import QR_cvx
+n, d, random_state = 1000, 50, 0
 
-iter_range = np.arange(50, 10000, 50)
+iter_range = np.arange(500, 50000, 500)
+tol_range = 10**np.arange(-10, -3, 0.3)
 
 def run_example(df, n=3000, d=10, random_state=0):
     np.random.seed(random_state)
@@ -22,61 +25,64 @@ def run_example(df, n=3000, d=10, random_state=0):
 
     X = np.random.randn(n,d)
     beta0 = np.random.randn(d)
-    beta0 = beta0 / np.sum(beta0**2)
     y = X@beta0 + np.random.rand()*np.random.randn(n)
 
+    out0 = X@beta0
+    obj0 = obj(C=1., y=y, out=out0, loss={'name':'QR', 'qt': [q]})
+
+    l2_term = np.sum(beta0**2) / 2
+    C = l2_term / obj0
+    lam1 = l2_term / np.sum(abs(beta0))
     # X, y = make_regression(n_samples=n, n_features=d, n_informative=d, noise=np.random.rand(), random_state=random_state)
     ## true solution
-    model = QuantReg(y, sm.add_constant(X))
-    res = model.fit(q=q, max_iter=10000000, p_tol=1e-8)
-    out_true = res.params[0] + X @ res.params[1:]
+    # https://www.cvxpy.org/tutorial/advanced/index.html
+    beta0, _ = QR_cvx(np.c_[X, np.ones(n)], y, lam1=lam1/C, lam2=1./C, q=q, solver_config={'solver':'ECOS', 'max_iters':10000})
     
-    obj0 = obj(C=1., y=y, out=out_true, loss={'name':'QR', 'qt': [q]})
+    out0 = X@beta0[:-1] + beta0[-1]
+    obj0 = obj(C=C, y=y, out=out0, loss={'name':'QR', 'qt': [q]}) + 0.5*np.sum(beta0**2) + lam1*np.sum(abs(beta0))
+    
 
     print('-'*25)
-    print('Minmizing via QuantReg(iterative reweighted least squares)')
+    print('Minmizing via ECOS')
     print('-'*25)
 
-    for max_iter_tmp in iter_range:
-        model_tmp = QuantReg(y, sm.add_constant(X))
-        
-        st = time.time()
-        res = model.fit(q=q, max_iter=max_iter_tmp, p_tol=1e-10)
-        et = time.time()
-        
-        out_sm = res.params[0] + X @ res.params[1:]
-        obj_sm = obj(C=1., y=y, out=out_sm, loss={'name':'QR', 'qt': [q]})
+    for max_iter_tmp in iter_range:        
+        beta_ECOS, time_ECOS = QR_cvx(np.c_[X, np.ones(n)], y, lam1=lam1/C, lam2=1./C, q=q, solver_config={'solver':'ECOS', 'max_iters':max_iter_tmp})
 
-        err_tmp = obj_sm - obj0
+        out_ECOS = beta_ECOS[-1] + X @ beta_ECOS[:-1]
+        obj_ECOS = obj(C=C, y=y, out=out_ECOS, loss={'name':'QR', 'qt': [q]}) + 0.5*np.sum(beta_ECOS**2) + lam1*np.sum(abs(beta_ECOS))
+
+        err_tmp = obj_ECOS - obj0
 
         if err_tmp <= 1e-5:
-            df = _append_result(df, n, d, 1.0, err_tmp, "QuantReg", et-st)
+            df = _append_result(df, n, d, C, err_tmp, "ECOS", time_ECOS)
+            print('ECOS ends with max_iter: %d' %max_iter_tmp)
             break
 
     print('-'*25)
     print('Minmizing via ReHLine')
     print('-'*25)
 
-    for c_tmp in [.001, 0.1, 1, 10, 100, 1000, 10000]:
-        for max_iter_tmp in iter_range:
-            cue = ReHLine(C=c_tmp, verbose=False, tol=1e-10, max_iter=max_iter_tmp)
-            X_fake=cue.make_ReLHLoss(X=X, y=y, loss={'name':'QR', 'qt':[q]})
+    for max_iter_tmp in iter_range:
+        cue = ReHLine(C=C, verbose=False, tol=1e-7, max_iter=max_iter_tmp)
+        X_fake=cue.make_ReLHLoss(X=X, y=y, loss={'name':'QR', 'qt':[q]})
+        X_fake=cue.append_l1(X_fake, l1_pen=lam1)
 
-            st = time.time()
-            cue.fit(X_fake)
-            et = time.time()
+        st = time.time()
+        cue.fit(X_fake)
+        et = time.time()
 
-            out_rehl = X @ cue.coef_[:-1] + cue.coef_[-1]
-            obj_rehl = obj(C=1., y=y, out=out_rehl, loss={'name':'QR', 'qt': [q]})
+        out_rehl = X @ cue.coef_[:-1] + cue.coef_[-1]
+        obj_rehl = obj(C=C, y=y, out=out_rehl, loss={'name':'QR', 'qt': [q]}) + 0.5*np.sum(cue.coef_**2) + lam1*np.sum(abs(cue.coef_))
 
-            err_tmp = obj_rehl - obj0
+        err_tmp = obj_rehl - obj0
+        print('ReHLine: err: %.3f' %err_tmp)
 
-            if err_tmp <= 1e-5:
-                df = _append_result(df, n, d, c_tmp, err_tmp, "ReHLine", et-st)
-                break
-        
         if err_tmp <= 1e-5:
+            df = _append_result(df, n, d, C, err_tmp, "ReHLine", et-st)
+            print('ReHLine ends with max_iter: %d' %max_iter_tmp)
             break
+        
     return df
 
 
