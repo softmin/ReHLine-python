af575c601a6e38737ea715435fbf65187faceef4
statmlben <bendaimath@gmail.com>
2023-04-11 20:32:09 +0800

    #QR: ridge

--- python/benchmark/QR/funs.py
+++ python/benchmark/QR/funs.py
@@ -8,11 +8,10 @@ def elastic_net(betas, lambda_l1, lambda_l2):
     return lambda_l1 * cvx.pnorm(betas, p=1) + \
            lambda_l2 / 2.0 * cvx.pnorm(betas, p=2)**2
 
-def regularized_loss(betas, lambda_l1, lambda_l2, X, y):
-    return mse(betas, X, y) + \
-           elastic_net(betas, lambda_l1, lambda_l2)
+def l2_pen(betas, lambda_l2):
+    return lambda_l2 / 2.0 * cvx.pnorm(betas, p=2)**2
 
-def QR_cvx(X_train, y_train, lam1, lam2, q, solver_config):
+def cvxQR_elastic(X_train, y_train, lam1, lam2, q, solver_config):
     u = cvx.Variable(X_train.shape[0], nonneg=True)
     v = cvx.Variable(X_train.shape[0], nonneg=True)
     tau = cvx.Parameter()
@@ -25,6 +24,26 @@ def QR_cvx(X_train, y_train, lam1, lam2, q, solver_config):
     lambda_l2.value = lam2
 
     objective = cvx.sum(tau * u) + cvx.sum((1-tau) * v) + elastic_net(betas, lambda_l1, lambda_l2)
+    constraints = [cvx.matmul(X_train, betas) + u - v == y_train]
+
+    problem = cvx.Problem(cvx.Minimize(objective), constraints)
+    st = time.time()
+    problem.solve(**solver_config)
+    et = time.time()
+
+    return betas.value, et - st
+
+def cvxQR_l2(X_train, y_train, lam2, q, solver_config):
+    u = cvx.Variable(X_train.shape[0], nonneg=True)
+    v = cvx.Variable(X_train.shape[0], nonneg=True)
+    tau = cvx.Parameter()
+    betas = cvx.Variable(shape=X_train.shape[1])
+    lambda_l2 = cvx.Parameter(nonneg=True)
+
+    tau.value = q
+    lambda_l2.value = lam2
+
+    objective = cvx.sum(tau * u) + cvx.sum((1-tau) * v) + l2_pen(betas, lambda_l2)
     constraints = [cvx.matmul(X_train, betas) + u - v == y_train]
 
     problem = cvx.Problem(cvx.Minimize(objective), constraints)
--- python/benchmark/QR/test_l2.py
+++ python/benchmark/QR/test_l2.py
@@ -14,11 +14,12 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
 from funs import cvxQR_elastic, cvxQR_l2
-tol = 1e-4
+tol = 0.01
 
 n, d, random_state = 1000, 100, 1
 
-iter_range = np.arange(1000, 50000, 1000)
+iter_range = 10**np.arange(3, 8, 0.2)
+iter_range = np.array(iter_range, dtype=int)
 tol_range = 10**np.arange(-10, -3, 0.3)
 
 def run_example(df, n=3000, d=10, random_state=0):
@@ -27,21 +28,22 @@ def run_example(df, n=3000, d=10, random_state=0):
 
     X = np.random.randn(n,d)
     beta0 = np.random.randn(d)
-    y = X@beta0 + np.random.rand()*np.random.randn(n)
+    y = X@beta0 + 0.1*np.random.randn(n)
+    # X, y, beta0 = make_regression(n_samples=n, n_features=d, n_informative=int(d/2), noise=np.random.rand(), coef=True, random_state=random_state)
 
-    out0 = X@beta0
-    obj0 = obj(C=1., y=y, out=out0, loss={'name':'QR', 'qt': [q]})
+    # out0 = X@beta0
+    # obj0 = obj(C=1., y=y, out=out0, loss={'name':'QR', 'qt': [q]})
 
-    l2_term = np.sum(beta0**2) / 2
-    C = l2_term / obj0
-    lam1 = l2_term / np.sum(abs(beta0))
-    # X, y = make_regression(n_samples=n, n_features=d, n_informative=d, noise=np.random.rand(), random_state=random_state)
+    # l2_term = np.sum(beta0**2) / 2
+    # C = l2_term / obj0
+    C = 1.0
+
     ## true solution
     # https://www.cvxpy.org/tutorial/advanced/index.html
-    beta0, _ = QR_cvx(np.c_[X, np.ones(n)], y, lam1=lam1/C, lam2=1./C, q=q, solver_config={'solver':'ECOS', 'max_iters':10000})
+    beta0, _ = cvxQR_l2(np.c_[X, np.ones(n)], y, lam2=1./C, q=q, solver_config={'solver':'ECOS', 'max_iters':10000})
     
     out0 = X@beta0[:-1] + beta0[-1]
-    obj0 = obj(C=C, y=y, out=out0, loss={'name':'QR', 'qt': [q]}) + 0.5*np.sum(beta0**2) + lam1*np.sum(abs(beta0))
+    obj0 = obj(C=C, y=y, out=out0, loss={'name':'QR', 'qt': [q]}) + 0.5*np.sum(beta0**2)
     
 
     print('-'*25)
@@ -49,10 +51,10 @@ def run_example(df, n=3000, d=10, random_state=0):
     print('-'*25)
 
     for max_iter_tmp in iter_range:        
-        beta_ECOS, time_ECOS = QR_cvx(np.c_[X, np.ones(n)], y, lam1=lam1/C, lam2=1./C, q=q, solver_config={'solver':'ECOS', 'max_iters':max_iter_tmp})
+        beta_ECOS, time_ECOS = cvxQR_l2(np.c_[X, np.ones(n)], y, lam2=1./C, q=q, solver_config={'solver':'ECOS', 'max_iters':max_iter_tmp})
 
         out_ECOS = beta_ECOS[-1] + X @ beta_ECOS[:-1]
-        obj_ECOS = obj(C=C, y=y, out=out_ECOS, loss={'name':'QR', 'qt': [q]}) + 0.5*np.sum(beta_ECOS**2) + lam1*np.sum(abs(beta_ECOS))
+        obj_ECOS = obj(C=C, y=y, out=out_ECOS, loss={'name':'QR', 'qt': [q]}) + 0.5*np.sum(beta_ECOS**2)
 
         err_tmp = (obj_ECOS - obj0) / obj0
 
@@ -66,16 +68,15 @@ def run_example(df, n=3000, d=10, random_state=0):
     print('-'*25)
 
     for max_iter_tmp in iter_range:
-        cue = ReHLine(C=C, verbose=False, tol=1e-6, max_iter=max_iter_tmp)
+        cue = ReHLine(C=C, verbose=False, tol=1e-7, max_iter=max_iter_tmp)
         X_fake=cue.make_ReLHLoss(X=X, y=y, loss={'name':'QR', 'qt':[q]})
-        X_fake=cue.append_l1(X_fake, l1_pen=lam1)
 
         st = time.time()
         cue.fit(X_fake)
         et = time.time()
 
         out_rehl = X @ cue.coef_[:-1] + cue.coef_[-1]
-        obj_rehl = obj(C=C, y=y, out=out_rehl, loss={'name':'QR', 'qt': [q]}) + 0.5*np.sum(cue.coef_**2) + lam1*np.sum(abs(cue.coef_))
+        obj_rehl = obj(C=C, y=y, out=out_rehl, loss={'name':'QR', 'qt': [q]}) + 0.5*np.sum(cue.coef_**2)
 
         err_tmp = (obj_rehl - obj0) / obj0
         print('ReHLine: err: %.3f' %err_tmp)
@@ -84,17 +85,16 @@ def run_example(df, n=3000, d=10, random_state=0):
             df = _append_result(df, n, d, C, err_tmp, "ReHLine", et-st)
             print('ReHLine ends with max_iter: %d' %max_iter_tmp)
             break
-        
     return df
 
 
 if __name__ == '__main__':
 
     df = {'err': [], 'method': [], 'time': [], 'neg_log_err': [], 'n': [], 'dim': [], 'C': []}
-    for n in [1000]:
+    for n in [10000, 20000, 30000, 40000, 50000, 60000]:
     # for n in [50000, 100000, 150000, 200000, 250000, 300000, 350000, 400000, 450000, 500000]:
         for d in [100]:
-            for i in range(20):
+            for i in range(10):
                 df = run_example(df, n=n, d=d, random_state=i)
                 print(pd.DataFrame(df).to_string())
 
